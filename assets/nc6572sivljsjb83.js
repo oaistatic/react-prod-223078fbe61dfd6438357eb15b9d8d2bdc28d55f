a04047c81b16738ae382a4bb8396592c96922768
lyy0709 <2021011603@student.cup.edu.cn>
2025-06-11 22:23:09 +0800

    update:react-prod-223078fbe61dfd6438357eb15b9d8d2bdc28d55f

--- assets/nc6572sivljsjb83.js
+++ assets/nc6572sivljsjb83.js
@@ -1,2 +1,2 @@
-const t="{{.assetPrefix}}/assets/made-with-chatgpt-og-g73iyayd.webp",s={src:t};export{s as m};
+const t="/assets/made-with-chatgpt-og-g73iyayd.webp",s={src:t};export{s as m};
 //# sourceMappingURL=nc6572sivljsjb83.js.map
